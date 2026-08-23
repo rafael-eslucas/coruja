bdcb49680945be1adf7688312ddc7eae17c3e740
Rafael <rafael.eslucas@gmail.com>
2026-08-23 13:11:45 -0300

    O último commit foi pela metade

--- frontend/patients.js
+++ frontend/patients.js
@@ -1,6 +1,7 @@
-const form = document.querySelector("form");
+const addpatient = document.querySelector("#addpatient");
+const addtutor = document.querySelector("#addtutor");
 
-form.addEventListener("submit", async event => {
+addpatient.addEventListener("submit", async event => {
     event.preventDefault();
 
     const form = event.currentTarget;
@@ -9,12 +10,12 @@ form.addEventListener("submit", async event => {
         "name": form.name.value,
         "species": form.species.value,
         "breed": form.breed.value,
-        "birth": form.birth.value,
-        "notes": form.notes.value,
+        "birth": form.birth.value === "" ? null : form.birth.value,
+        "notes": form.notes.value === "" ? null : form.birth.value,
         "owner_id": form.owner.value
     };
 
-    const response = await fetch("/api/patients", {
+    const response = await fetch("/api/addpatient", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -22,10 +23,33 @@ form.addEventListener("submit", async event => {
         body: JSON.stringify(info)
     });
 
-    const made = await response.json();
+/*    const made = await response.json();
 
     console.log(made.success);
     if (logged.authorized) {
         window.location.href = "/patients";
-    }
+    }*/
+});
+
+addtutor.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+
+    const info = {
+        "name": form.name.value,
+        "phone": form.phone.value === "" ? null : form.phone.value,
+        "city": form.city.value,
+        "neighborhood": form.neighborhood.value,
+        "street": form.street.value,
+        "number": form.number.value
+    };
+
+    const response = await fetch("/api/addowner", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(info)
+    });
 });
